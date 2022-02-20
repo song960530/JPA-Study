ee87139e49aaf56a919abd9342f6e2441d84affb
송문준 <song960530@naver.com>
2022-02-20 16:45:45 +0900

    feat: 주문상태 검색을 위한 함수 틀을 만들어라

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor // Spring Data Jpa를 사용하면 PersistenceContext도 생성자를 통해 자동으로 주입해준다
@@ -19,6 +21,10 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll (OrderSearch orderSearch){}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // JPQL, Criteria를 사용하여 동적쿼리 생성이 가능하지만 유지보수성이 급격하게 떨어지므로 동적쿼리에선 쿼리QSL을 권장
+        // 여기서도 따로 적진않고 쿼리QSL로 작성하겠음.
+        return null;
+    }
 
 }
