c1173c811ec8e3097412210d0f8771b86884f122
송문준 <song960530@naver.com>
2022-02-20 13:25:14 +0900

    feat: 주문Repository를 생성하라

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor // Spring Data Jpa를 사용하면 PersistenceContext도 생성자를 통해 자동으로 주입해준다
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll (OrderSearch orderSearch){}
+
+}
