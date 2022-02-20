b1bdadfe12fef55c054816de6374ca84b7559bf1
송문준 <song960530@naver.com>
2022-02-20 17:23:14 +0900

    feat: 기능 테스트를 위한 화면을 구성하라

--- jpashop/src/main/java/jpabook/jpashop/controller/HelloController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -1,16 +1,25 @@
-package jpabook.jpashop.web;
+package jpabook.jpashop.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @RequestMapping
+    public String home() {
+        log.debug("home Controller");
+        return "home";
     }
 
 }
