31707936b09657d637bfdd66bff45884da25b981
송문준 <song960530@naver.com>
2022-02-27 23:35:56 +0900

    feat: 순수 JPA 페이징을 추가하라

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -41,4 +41,18 @@ public class MemberJpaRepository {
         em.remove(member);
     }
 
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username asc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
+
 }
