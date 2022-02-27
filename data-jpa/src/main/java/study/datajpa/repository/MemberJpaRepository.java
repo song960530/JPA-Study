bf4dc6d320d88b6a49f4c6663af451a13162bfc6
송문준 <song960530@naver.com>
2022-02-27 13:47:47 +0900

    feat: CRUD 기능을 직접 작성하라

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,6 +22,23 @@ public class MemberJpaRepository {
     public Member find(Long id) {
         Member member = em.find(Member.class, id);
         return member;
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
     }
 
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,11 +1,16 @@
 package study.datajpa.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -14,6 +19,7 @@ class MemberJpaRepositoryTest {
     MemberJpaRepository memberJpaRepository;
 
     @Test
+    @DisplayName("기본 저장,검색기능")
     public void testMember() throws Exception {
         // given
         Member member = new Member("memberA");
@@ -23,9 +29,38 @@ class MemberJpaRepositoryTest {
         Member findMember = memberJpaRepository.find(save.getId());
 
         // then
-        Assertions.assertEquals(save.getId(), findMember.getId());
-        Assertions.assertEquals(save.getUsername(), findMember.getUsername());
-        Assertions.assertEquals(save, findMember);
+        assertEquals(save.getId(), findMember.getId());
+        assertEquals(save.getUsername(), findMember.getUsername());
+        assertEquals(save, findMember);
     }
 
+    @Test
+    @DisplayName("CRUD테스트")
+    public void basicCRUD() throws Exception {
+        // given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // when
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        List<Member> all = memberJpaRepository.findAll();
+        Long count = memberJpaRepository.count();
+
+        // then
+        assertEquals(member1, findMember1);
+        assertEquals(member2, findMember2);
+
+        assertEquals(all.size(), 2);
+
+        assertEquals(count, 2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        Long deleteCount = memberJpaRepository.count();
+        assertEquals(deleteCount, 0);
+    }
 }
