ba813235fc32d4364f28c88910a48e129f9b1fdb
송문준 <song960530@naver.com>
2022-03-02 00:47:59 +0900

    feat: Q파일의 동작을 확인하라

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertEquals(result, hello);
+        assertEquals(result.getId(), result.getId());
+    }
 
 }
